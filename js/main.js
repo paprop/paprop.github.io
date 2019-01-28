0050a244d05dcb48f970955dd6dbb29e3c3b1000
paprop <pavoniaavenueproperties@gmail.com>
2019-01-28 03:13:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 $(document).ready(function($) {
+
   $('ul li:nth-child(1)').click(function(){
     $('html, body').animate({
-    scrollTop: $(".top-navigation").offset().top
-}, 250);
+    scrollTop: $(".introduction").offset().top
+}, 400);
   });
     $('ul li:nth-child(2)').click(function(){
     $('html, body').animate({
-    scrollTop: $(".properties").offset().top
-}, 250);
+    scrollTop: $(".about").offset().top
+}, 400);
     });
         $('ul li:nth-child(3)').click(function(){
     $('html, body').animate({
-    scrollTop: $(".properties1").offset().top
-}, 250);
+    scrollTop: $(".investments").offset().top
+}, 400);
     });
       $('ul li:nth-child(4)').click(function(){
     $('html, body').animate({
+    scrollTop: $(".criteria").offset().top
+}, 400);
+    });
+
+     $('ul li:nth-child(5)').click(function(){
+    $('html, body').animate({
     scrollTop: $(".contact").offset().top
-}, 250);
+}, 400);
     });
   
 });
